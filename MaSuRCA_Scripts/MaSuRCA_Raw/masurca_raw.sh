9fc09547f82849833fe2983efaeda99ec1a73b8e
kara-heilemann <kara.heilemann@gmail.com>
2021-05-05 16:30:02 -0400

    adding script directories

---
+++ MaSuRCA_Scripts/MaSuRCA_Raw/masurca_raw.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=masurca1
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 16
+#SBATCH --mem=200G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=kara.heileman@uconn.edu
+#SBATCH -o %x_%A.out
+#SBATCH -e %x_%A.err
+
+hostname
+date
+
+##########################################################
+##              MASuRCA                                 ##
+##########################################################
+module load perl/5.28.1
+
+module load MaSuRCA/3.3.4 
+
+masurca config_file
+
+bash assemble.sh
+
+module unload MaSuRCA/3.3.4
+
---
+++ MaSuRCA_Scripts/MaSuRCA_RawUn/masurca_ru.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=masurca_tuh1
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 16
+#SBATCH --mem=200G
+#SBATCH --partition=himem
+#SBATCH --qos=himem
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=kara.heilemann@uconn.edu
+#SBATCH -o %x_%A.out
+#SBATCH -e %x_%A.err
+
+hostname
+date
+
+##########################################################
+##              MASuRCA                                 ##
+##########################################################
+module load perl/5.28.1
+
+module load MaSuRCA/3.3.4 
+
+masurca config_file
+
+bash assemble.sh
+
+module unload MaSuRCA/3.3.4
